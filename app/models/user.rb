040e406160fd90403f632984450a973df9c7e5e3
naojirouhisada <hn1213sx00vytr@yahoo.co.jp>
2015-08-28 06:30:00 +0000

    added want have

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,12 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
+  
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items, through: :wants, source: :item
+  
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items, through: :haves, source: :item
 
 
   # 他のユーザーをフォローする
@@ -29,22 +35,34 @@ class User < ActiveRecord::Base
     following_users.include?(other_user)
   end
 
-  ## TODO 実装
+  ## 
+   #アイテムをhaveにする
   def have(item)
+    haves.create(item_id: item.id)
   end
-
+  
+  #アイテムをunhaveにする
   def unhave(item)
+    haves.find_by(item_id: item.id).destroy
   end
-
+  
+  #アイテムをhaveしているかどうか
   def have?(item)
+    have_items.include?(item)
   end
-
+  
+  #アイテムをwantにする
   def want(item)
+    wants.create(item_id: item.id)
   end
-
+  
+  #アイテムをunwantにする
   def unwant(item)
+    wants.find_by(item_id: item.id).destroy
   end
-
+  
+  #アイテムをwantしているかどうか
   def want?(item)
+    want_items.include?(item)
   end
 end
