9e6eded653aa8b2479330c04805127ba1db6199a
naojirouhisada <hn1213sx00vytr@yahoo.co.jp>
2015-08-29 13:45:23 +0000

    added have_and_want_btn

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,7 +12,9 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = {}
+        response = Amazon::Ecs.item_lookup(params[:asin], 
+                                  :response_group => 'Medium' , 
+                                  :country => 'jp')
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
@@ -31,20 +33,25 @@ class OwnershipsController < ApplicationController
     # params[:type]の値ににHaveボタンが押された時にはの時は「Have」,
     # Wantボタンがされた時には「Want」が設定されています。
     
-    current_user.have(@item)
-    current_user.want(@item)
+    if params[:type] == 'Have'
+      current_user.have(@item)
+    elsif params[:type] == 'Want'
+      current_user.want(@item)
+    end
 
   end
 
   def destroy
-    @item = Item.find(params[:item_id]).item
+     @item = Item.find(params[:item_id])
+     
+     if params[:type] == 'Have'
+       current_user.unhave(@item)
+     elsif params[:type] == 'Want'
+       current_user.unwant(@item)
+     end
 
     # TODO 紐付けの解除。 
     # params[:type]の値ににHavedボタンが押された時にはの時は「Have」,
-    # Wantedボタンがされた時には「Want」が設定されています。
-    
-    current_user.unhave(@item)
-    current_user.unwant(@item)
-
+    # Wantedボタンがされた時には「Want」が設定されています
   end
 end
